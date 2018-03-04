474a5d655a8b701ce6606c96ce4f4dad761616c0
Thomas Cheng <thomascheng1998@googlemail.com>
2018-03-04 02:22:30 +0000

    Benched and optimised searching

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -52,7 +52,7 @@ impl SearchBuffer {
     /// Given a string, return a list of string slices of which the input is a subsequence of.
     pub fn find_all_subsequences(&self, search: &str) -> Vec<&str> {
         let search = &search.to_lowercase();
-        let mut results = Vec::new();
+        let mut results = Vec::with_capacity(1000);
         // Hold a reference into the search string, advance each time a char is a match
         let mut curr_search_iter = search.chars();
         let mut curr_base = 0; // Base ix of the curr string in buf
@@ -70,12 +70,99 @@ impl SearchBuffer {
             }
             // Not a null char, so check if we can advance curr_search_ix
             if curr_search_iter.as_str().len() > 0 &&
-                c.to_lowercase().next().unwrap() ==
+                c.to_ascii_lowercase() ==
                     curr_search_iter.as_str().chars().next().unwrap()
             {
                 curr_search_iter.next().unwrap();
             }
         }
         return results;
+    }
+
+    pub fn find_all_containing_char(&self, search: char) -> Vec<&str> {
+        let search = search.to_ascii_lowercase();
+        let mut results = Vec::with_capacity(1000);
+        let mut curr_base = 0; // Base ix of the curr string in buf
+        let mut skipping = false;
+        for (ii, c) in self.buf.char_indices() {
+            if c == '\x00' {
+                skipping = false;
+                curr_base = ii + 1;
+            }
+            if skipping { continue; }
+            // Not a null char, so check if we can advance curr_search_ix
+            if search == c.to_ascii_lowercase() {
+                unsafe {
+                    results.push(self.buf.slice_unchecked(curr_base, ii));
+                }
+                skipping = true;
+            }
+        }
+        return results;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test;
+
+    /** Test searching through a given amount of 30 char items */
+    fn bench_search(b: &mut test::Bencher, num_classes: usize) {
+        const SEARCH_TERM : &'static str = "Al£adfAff";
+        let mut search_buffer = SearchBuffer::new();
+        search_buffer.add_strings(&vec!["ALKJasdlf.£asdwhio.fASdhsdfo."; num_classes][..]);
+        b.iter(|| search_buffer.find_all_subsequences(SEARCH_TERM));
+    }
+
+    #[bench]
+    /** Test searching through enough data to overflow cache exactly (3mb) */
+    fn bench_search_0_125x_cache(b: &mut test::Bencher) {
+        bench_search(b, 12500);
+    }
+
+    #[bench]
+    /** Test searching through enough data to overflow cache exactly (3mb) */
+    fn bench_search_0_25x_cache(b: &mut test::Bencher) {
+        bench_search(b, 25000);
+    }
+
+    #[bench]
+    /** Test searching through enough data to overflow cache exactly (3mb) */
+    fn bench_search_0_5x_cache(b: &mut test::Bencher) {
+        bench_search(b, 50000);
+    }
+
+
+    #[bench]
+    /** Test searching through enough data to overflow cache exactly (3mb) */
+    fn bench_search_1x_cache(b: &mut test::Bencher) {
+        bench_search(b, 100000);
+    }
+
+    #[bench]
+    /** Test searching through enough data to overflow cache by 2x (6mb) */
+    fn bench_search_2x_cache(b: &mut test::Bencher) {
+        bench_search(b, 200000);
+    }
+
+    #[bench]
+    /** Test searching through enough data to overflow cache by 3x (9mb) */
+    fn bench_search_3x_cache(b: &mut test::Bencher) {
+        bench_search(b, 300000);
+    }
+
+    #[bench]
+    /** Test searching for a char through loads of 18 char items, then placing the results in a search
+     * buffer */
+    fn bench_char_search_1000_and_alloc(b: &mut test::Bencher) {
+        let mut search_buffer = SearchBuffer::new();
+        search_buffer.add_strings(&vec!["ABCDEFJALSKQOEMWND"; 50000][..]);
+        b.iter(|| {
+            let res = search_buffer.find_all_containing_char('n');
+            let mut new_search_buffer = SearchBuffer::new();
+            new_search_buffer.add_strings(&res[..]);
+            test::black_box(new_search_buffer);
+        });
     }
 }
